db9ce6fff5d086c25c632147d0c7cfc9470e8d03
didacusabella <diequito93@gmail.com>
2018-04-26 15:54:43 +0200

    fix job name

--- src/main/java/it/unisa/soa/hadoopworkspace/WordCount.java
+++ src/main/java/it/unisa/soa/hadoopworkspace/WordCount.java
@@ -53,7 +53,7 @@ public class WordCount {
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "word count");
+    Job job = Job.getInstance(conf, "word_count");
     job.setJarByClass(WordCount.class);
     job.setMapperClass(TokenizerMapper.class);
     job.setCombinerClass(IntSumReducer.class);
